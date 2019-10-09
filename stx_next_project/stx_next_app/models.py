2abd41bb723bd6a173062a13282c4174e86efbb8
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-10-09 21:14:22 +0200

    author in model as TextField

--- stx_next_project/stx_next_app/models.py
+++ stx_next_project/stx_next_app/models.py
@@ -1,24 +1,20 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.TextField()
+    authors = models.TextField()
     published_date = models.DateTimeField()
     page_count = models.IntegerField()
     image_links = models.ForeignKey('ImageLinks', on_delete=models.DO_NOTHING)
     language = models.CharField(max_length=10)
 
-class Author(models.Model):
-    author = models.CharField(max_length=100)
-    book = models.ManyToManyField('Book', through='BookAuthors')
-
-class BookAuthors(models.Model):
-    author = models.ForeignKey('Author', on_delete=models.DO_NOTHING)
-    book = models.ForeignKey('Book', on_delete=models.DO_NOTHING)
 
 class IndustryIdentifiers(models.Model):
     type = models.TextField()
     identifier = models.TextField()
     book = models.ForeignKey('Book', on_delete=models.DO_NOTHING)
+
 
 class ImageLinks(models.Model):
     small_thumbnail = models.URLField()
