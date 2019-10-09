37c81d040b57df5ab25a7149a99c33f8cc479f5b
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-10-09 21:08:14 +0200

    book add init

--- stx_next_project/stx_next_app/views.py
+++ stx_next_project/stx_next_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 from .models import Book,Author,ImageLinks,IndustryIdentifiers,BookAuthors
 
 class BookList(ListView):
